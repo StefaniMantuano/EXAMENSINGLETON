30628bd5c45d7d281c53a9baa1c075acb55335bb
StefaniMantuano <mantuanostefania15@gmail.com>
2022-01-21 22:07:36 -0500

    Singleton

---
+++ SingletonPrueba/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPrueba
+{
+    class Program
+    { 
+      static void Main(string[] args)
+    {
+            Console.WriteLine(Singleton.Instance.mensaje);
+            Singleton.Instance.mensaje = "HOLA USUARIO";
+
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+
+    }
+}
+}
